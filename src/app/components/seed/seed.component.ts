03d050536342011fdba3efc94a6c33a2bd9fd8fe
Sebastian Garcia <js.garcial1@uniandes.edu.co>
2018-07-18 15:32:33 -0500

    post issue fixed

--- src/app/components/seed/seed.component.ts
+++ src/app/components/seed/seed.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { Seed } from '../../DTOS/seed';
 import { Reply } from '../../DTOS/reply';
 import { AuthorsService } from '../../services/authors.service';
+import { Author } from '../../DTOS/author';
 
 @Component({
   selector: 'app-seed',
@@ -19,13 +20,21 @@ export class SeedComponent implements OnInit {
   private s: Seed;
   public replies: Reply[];
   public new_reply: string = '';
+  private arturito: Author;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private social_service: SocialService,
     public auth: AuthorsService
-  ) { }
+  ) {
+    if (this.auth.author) {
+      this.auth.author.subscribe(ar => {
+        this.arturito = ar;
+        return;
+      });
+    }
+   }
 
   ngOnInit() {
     this.get_seed(this.route.snapshot.paramMap.get('id'));
@@ -34,10 +43,9 @@ export class SeedComponent implements OnInit {
 
 
   reply(): void {
-    if (this.new_reply !== '' && this.new_reply !== undefined && this.new_reply !== null) {
+    if (this.new_reply !== '' && this.new_reply !== undefined && this.new_reply !== null && this.auth.author) {
       let x: Reply = {
-        //author,
-        // author_image
+        author: this.arturito.displayName,
         date: '' + new Date().toLocaleString(),
         text: this.new_reply,
         ups: 0
--- src/app/components/social/social.component.ts
+++ src/app/components/social/social.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { Seed } from '../../DTOS/seed';
 import { Observable, of } from 'rxjs';
 import { AuthorsService } from '../../services/authors.service';
+import { Author } from '../../DTOS/author';
 
 @Component({
   selector: 'app-social',
@@ -21,12 +22,21 @@ export class SocialComponent implements OnInit {
   intenta_votar: boolean;
   admin: boolean;
 
+  private arturito: Author;
+
   constructor(
     private social_servicce: SocialService,
     private route: ActivatedRoute,
     private location: Location,
     public auth: AuthorsService
-  ) { }
+  ) {
+    if (this.auth.author) {
+      this.auth.author.subscribe(ar => {
+        this.arturito = ar;
+        return;
+      });
+    }
+  }
 
   ngOnInit() {
     this.admin = true
@@ -35,31 +45,33 @@ export class SocialComponent implements OnInit {
   }
 
   onSubmit({ value, valid }: { value: Seed, valid: boolean }): void {
-    if (value.text === undefined || value.text === '') {
-      value.text = '';
+    if (this.auth.author) {
+      if (value.text === undefined || value.text === '') {
+        value.text = '';
+      }
+      if (value.image === undefined || value.image === '') {
+        value.image = '';
+      }
+      value.author = this.arturito.displayName;
+      value.author_image = this.arturito.photoURL;
+      value.date = '' + new Date().toLocaleString();
+      value.replies = [];
+      value.ups = 0;
+      this.social_servicce.post_seed(value);
+      this.posteado = !this.posteado;
+      this.form.reset();
     }
-    if (value.image === undefined || value.image === '') {
-      value.image = '';
-    }
-    value.author = 'Username';
-    value.author_image = 'https://image.ibb.co/cFgpOd/boy.png';
-    value.date = '' + new Date().toLocaleString();
-    value.replies = [];
-    value.ups = 0;
-    this.social_servicce.post_seed(value);
-    this.posteado = !this.posteado;
-    this.form.reset();
   }
 
   update(x: Seed): void {
     this.intenta_votar = true;
-    if ( this.auth.author ) {
+    if (this.auth.author) {
       this.social_servicce.put_seed(x);
     }
   }
 
   delete(id: string): void {
-    if ( confirm('Seguro que quieres eleminar esta publicación?') ) {
+    if (confirm('Seguro que quieres eleminar esta publicación?')) {
       this.social_servicce.delete_seed(id);
     }
   }
